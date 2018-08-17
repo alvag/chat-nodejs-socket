4ba7882bd36f53aae8b560504af5acdd80bbeea1
Max Alva <alva85@gmail.com>
2018-08-16 23:46:14 -0500

    Conexion al socket

--- server/index.js
+++ server/index.js
@@ -3,6 +3,12 @@ const app = express();
 const server = require( 'http' ).Server( app );
 const io = require( 'socket.io' )( server );
 
+app.use( express.static( 'client' ) );
+
+io.on( 'connection', ( socket ) => {
+	console.log( `Nueva conexión desde la IP ${socket.handshake.address}` );
+} );
+
 server.listen( 6677, () => {
 	console.log( 'Servidor corriendo en http://localhost:6677' );
 } );
